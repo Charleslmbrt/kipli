40f7a2d9cd71e539be7df1f3f20f5f2d3f40967b
Charleslmbrt <106771032+Charleslmbrt@users.noreply.github.com>
2023-07-11 19:28:10 +0200

    add banner component +add fonts + main.css

--- components/Banner.vue
+++ components/Banner.vue
@@ -1,6 +1,23 @@
 <template>
-    <div>
-<p>Banner Component</p>
+    <div class="flex justify-between items-center bg-[#314D3C] h-11 w-full px-3.5 text-xs tracking-[0.25em]">
+     <div class="notice flex items-center">
+      <div class="notice-stars flex mr-2">
+        <img class="w-3 h-3" src="/star.svg" alt="star 1">
+        <img class="w-3 h-3" src="/star.svg" alt="star 2">
+        <img class="w-3 h-3" src="/star.svg" alt="star 3">
+        <img class="w-3 h-3" src="/star.svg" alt="star 4">
+        <img class="w-3 h-3" src="/star.svg" alt="star 5">
+      </div>
+      <div class="notice-txt">4.8/5 sur 4.312 avis</div>
+     </div>
+     <p class="benefits hidden lg:block">
+     Livraison gratuite | Paiement 3x sans frais matelas | garanti 10 ans
+     </p>
+     <p class="lg:hidden">|</p>
+     <p class="phone-number">
+     +33 (0)1 76 42 11 72
+     </p>
+
     </div>
 </template>
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -20,4 +20,7 @@ export default {
     }
 }
 </script>
-<style></style>
+<style>
+
+
+</style>
